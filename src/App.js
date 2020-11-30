d9200605a77636451f7859b66d98846da814cb4c
19045884 <19045884@suning.com>
2020-12-01 00:47:05 +0800

    'init'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Row, Col} from 'antd'
+import NetFlow from './netflow.js'
+import Detail from './detail.js'
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="app">
+      <Row>
+        <Col span={16}><NetFlow /></Col>
+        <Col span={8}><Detail/></Col>
+      </Row>
     </div>
   );
 }
